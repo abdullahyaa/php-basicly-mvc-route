6759590a0caf8af890a469ba12974bf4848b1fe1
Abdullah AY <97941423+abdullahyaa@users.noreply.github.com>
2025-01-24 10:04:34 +0300

    Update view.php
    Adding renderComponent and start block

--- App/View/view.php
+++ App/View/view.php
@@ -1,14 +1,27 @@
-<?php
-
-namespace App\View;
-
 class View
 {
+    private static array $blocks = []; // Bloklar için
+    private static array $data = [];  // Component verileri için
+
+    public static function startBlock(string $name): void
+    {
+        ob_start();
+        self::$blocks[$name] = '';
+    }
+
+    public static function endBlock(string $name): void
+    {
+        if (array_key_exists($name, self::$blocks)) {
+            self::$blocks[$name] = ob_get_clean();
+        } else {
+            ob_end_clean();
+        }
+    }
+
     public static function render(string $template, array $data = [])
     {
         extract($data);
 
-        // İçerik şablonunu buffer ile yakala
         ob_start();
         $templatePath = $_SERVER['DOCUMENT_ROOT'] . "/mvc-architect/app/view/{$template}.php";
         if (file_exists($templatePath)) {
@@ -16,14 +29,30 @@ class View
         } else {
             echo "Template {$template}.php not found.";
         }
-        $content = ob_get_clean(); // Yakaladığımız içeriği $content'e al
+        $content = ob_get_clean();
 
-        // Base şablonu buffer ile yakala
         $basePath = $_SERVER['DOCUMENT_ROOT'] . "/mvc-architect/app/view/layout/base.php";
         if (file_exists($basePath)) {
             include $basePath;
         } else {
             echo "Base layout not found.";
         }
+    }
+
+    public static function renderComponent(string $component, array $data = []): void
+    {
+        extract($data);
+
+        $componentPath = $_SERVER['DOCUMENT_ROOT'] . "/mvc-architect/app/view/components/{$component}.php";
+        if (file_exists($componentPath)) {
+            include $componentPath;
+        } else {
+            echo "Component {$component}.php not found.";
+        }
+    }
+
+    public static function showBlock(string $name): void
+    {
+        echo self::$blocks[$name] ?? '';
     }
 }
